cb5e06ecf003b34914f544b24dc58736ee4ec426
Matias Garcia <mggimeno11@gmail.com>
2023-06-12 10:49:39 +0200

    Method getVersion and program working

--- src/main/java/co/empathy/academy/search/HelloWorldController.java
+++ src/main/java/co/empathy/academy/search/HelloWorldController.java
@@ -1,5 +1,8 @@
 package co.empathy.academy.search;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,25 +10,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.print.attribute.standard.Media;
-import java.util.Collections;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class HelloWorldController {
 
-    public String getElasticVersion(){
+    public String getElasticVersion() throws JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:9200"
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-        ResponseEntity<String> response = restTemplate.exchange(url, entity, HttpMethod.GET, String.class);
-        String responseBody = response.getBody();
+        String url = "http://localhost:9200";
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response.getBody());
+        JsonNode version = root.path("version");
+        JsonNode number = version.path("number");
+        String result = number.toString();
+        result = result.substring(1,7);
 
-
-        return responseBody;
+        return result;
     }
     @GetMapping("/greet/{name}")
     public String greet(@PathVariable String name){
@@ -33,12 +36,11 @@ public class HelloWorldController {
     }
 
     @GetMapping("/search")
-    public Map<String, String> search(@RequestParam(name="query") String query){
+    public Map<String, String> search(@RequestParam(name="query") String query) throws JsonProcessingException {
         String response = getElasticVersion();
         Map<String, String> data = new HashMap<>();
         data.put("query", query);
         data.put("response",response);
-        data.put("clusterName", response);
         return data;
     }
 }
